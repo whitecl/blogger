498af03d69c604c571f651c9d463f4d8a4588575
Conor Devine <conor.devine@thoughtbot.com>
2021-03-18 15:38:19 -0400

    Create app that allows users to make posts
    User can create an account and manage sessions.
    User can create and view posts

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :posts
+  resources :users
+  resources :sessions, only: %i[new create destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  root 'home#index'
+end
